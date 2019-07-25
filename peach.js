7525c0a7941fea4247bc669faff0fed0e3ac0840
peach <apeach05@gmail.com>
2019-07-25 17:19:43 +0800

    上課筆記
    上課筆記

--- peach.js
+++ peach.js
@@ -44,6 +44,8 @@ function checkTWID(id){
     return ret; //回傳至誰呼叫的
 }
 
+
+//週三作業
 function creatTWID(){
 
 }
@@ -69,3 +71,30 @@ function creatTWIDByAll(area, gender){
 //     }
 //     return ret;
 // }
+
+//數字遊戲
+function createAnswer(n = 3){
+    let poker = [];
+    for (let i = 0; i < 10; i++) poker[i] = i;
+    for (let i = poker.length - 1; i > 0; i--) {
+        let rand = parseInt(Math.random() * (i + 1));
+        [poker[i], poker[rand]] = [poker[rand], poker[i]];
+    }
+    let ret = '';
+    for(let i=0; i<n; i++){
+        ret += poker[i];
+    }
+    return ret;
+ }
+
+ function checKAB(ans, gus){
+     let a = 0, b = 0;
+     for (let i=0; i<gus.length; i++){
+         if (gus.charAt(i) == ans.charAt(i)){
+            a++;
+         }else if(ans.indexOf(gus.charAt(i)) >= 0){
+             b++;
+         }
+     }
+    return `${a}A${b}B`;
+ }
