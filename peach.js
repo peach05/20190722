081f1535712486305cf1cf00873317837d25c39f
peach <apeach05@gmail.com>
2019-07-26 17:57:59 +0800

    上課筆記
    上課筆記

--- peach.js
+++ peach.js
@@ -18,27 +18,27 @@ function sayYaV5(welcome, ...names) {
 
 
 // 身分證驗證
-function checkTWID(id){
+function checkTWID(id) {
     let ret = false;
     let regex = /^[A-Z][12][0-9]{8}$/;
-                                 //$代表結尾符號
+    //$代表結尾符號
     let letters = 'ABCDEFGHJKLMNPQRSTUVXYWZIO';
-    if (id.match(regex) != null){
+    if (id.match(regex) != null) {
         let c1 = id.charAt(0);
         let n12 = letters.indexOf(c1) + 10;
         let n1 = parseInt(n12 / 10);
         let n2 = n12 % 10;
-        let n3 = parseInt(id.substr(1,1));
-        let n4 = parseInt(id.substr(2,1));
-        let n5 = parseInt(id.substr(3,1));
-        let n6 = parseInt(id.substr(4,1));
-        let n7 = parseInt(id.substr(5,1));
-        let n8 = parseInt(id.substr(6,1));
-        let n9 = parseInt(id.substr(7,1));
-        let n10 = parseInt(id.substr(8,1));
-        let n11 = parseInt(id.substr(9,1));
-        let sum = n1*1 + n2*9 + n3*8 + n4*7 + n5*6 +
-            n6*5 + n7*4 + n8*3 + n9*2 + n10*1 + n11*1;
+        let n3 = parseInt(id.substr(1, 1));
+        let n4 = parseInt(id.substr(2, 1));
+        let n5 = parseInt(id.substr(3, 1));
+        let n6 = parseInt(id.substr(4, 1));
+        let n7 = parseInt(id.substr(5, 1));
+        let n8 = parseInt(id.substr(6, 1));
+        let n9 = parseInt(id.substr(7, 1));
+        let n10 = parseInt(id.substr(8, 1));
+        let n11 = parseInt(id.substr(9, 1));
+        let sum = n1 * 1 + n2 * 9 + n3 * 8 + n4 * 7 + n5 * 6 +
+            n6 * 5 + n7 * 4 + n8 * 3 + n9 * 2 + n10 * 1 + n11 * 1;
         ret = sum % 10 == 0;
     }
     return ret; //回傳至誰呼叫的
@@ -46,19 +46,19 @@ function checkTWID(id){
 
 
 //週三作業
-function creatTWID(){
+function creatTWID() {
 
 }
 
-function creatTWIDByArea(area){
+function creatTWIDByArea(area) {
 
 }
 
-function creatTWIDByGender(gender){
+function creatTWIDByGender(gender) {
 
 }
 
-function creatTWIDByAll(area, gender){
+function creatTWIDByAll(area, gender) {
 
 }
 
@@ -73,7 +73,7 @@ function creatTWIDByAll(area, gender){
 // }
 
 //數字遊戲
-function createAnswer(n = 3){
+function createAnswer(n = 3) {
     let poker = [];
     for (let i = 0; i < 10; i++) poker[i] = i;
     for (let i = poker.length - 1; i > 0; i--) {
@@ -81,20 +81,48 @@ function createAnswer(n = 3){
         [poker[i], poker[rand]] = [poker[rand], poker[i]];
     }
     let ret = '';
-    for(let i=0; i<n; i++){
+    for (let i = 0; i < n; i++) {
         ret += poker[i];
     }
     return ret;
- }
+}
 
- function checKAB(ans, gus){
-     let a = 0, b = 0;
-     for (let i=0; i<gus.length; i++){
-         if (gus.charAt(i) == ans.charAt(i)){
+function checKAB(ans, gus) {
+    let a = 0, b = 0;
+    for (let i = 0; i < gus.length; i++) {
+        if (gus.charAt(i) == ans.charAt(i)) {
             a++;
-         }else if(ans.indexOf(gus.charAt(i)) >= 0){
-             b++;
-         }
-     }
+        } else if (ans.indexOf(gus.charAt(i)) >= 0) {
+            b++;
+        }
+    }
     return `${a}A${b}B`;
- }
+}
+
+//for brad44.html
+
+function clone(source) {
+    if (source == null || typeof (source) != 'object') return null;
+
+    let target = new Object();
+    for (let attr in source) {
+        if (typeof (source[attr]) != 'object') {
+            target[attr] = source[attr];
+        } else {
+            target[attr] = clone(source[attr]);
+        }
+    }
+
+    return target;
+}
+
+//日期時間功能
+Date.prototype.getTWYear = function () {
+    return this.getFullYear() - 1911;
+}
+
+Date.prototype.getCWeek = function(){
+    let w = this.getDay();
+    let ws = ['禮拜日','禮拜一','禮拜二','禮拜三','禮拜四','禮拜五','禮拜六',];
+    return ws[w];
+}
